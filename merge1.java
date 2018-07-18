1bb66976647ef142dd8bd1888ce72664da7df7b0
geethuchowdary2528 <41250686+geethuchowdary2528@users.noreply.github.com>
2018-07-18 16:08:27 +0530

    merge1

---
+++ merge1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+public class merge1
+{
+
+public static void main(String args[])throws IOException
+{
+PrintWriter pw=new PrintWriter("file4.txt");
+BufferedReader br1=new BufferedReader(new FileReader("file1.txt"));
+BufferedReader br2=new BufferedReader(new FileReader("file2.txt"));
+String l1=br1.readLine();
+String l2=br2.readLine();
+while(l1!=null||l2!=null)
+{
+if(l1!=null)
+{
+pw.println(l1);
+l1=br1.readLine();
+}
+else
+{
+pw.println(l2);
+l2=br2.readLine();
+}
+}
+pw.flush();
+br1.close();
+br2.close();
+pw.close();
+System.out.println("merge is completed");
+}
+}
