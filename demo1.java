52a0fe30998d606c6722057b2704680b3be96268
geethuchowdary2528 <41250686+geethuchowdary2528@users.noreply.github.com>
2018-07-20 12:24:32 +0530

    task

---
+++ demo1.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class demo1 {
+	public demo1(int a,float b)
+	{
+		System.out.println(a);
+		System.out.println(b);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+demo1 d1=new demo1(14,3f);
+demo1 d2=new demo1(13,2f);
+
+	}
+
+}
