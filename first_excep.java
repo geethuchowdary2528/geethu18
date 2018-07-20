5fe0153e98cc09cbb759f0c3d4a83399a864b9cb
geethuchowdary2528 <41250686+geethuchowdary2528@users.noreply.github.com>
2018-07-20 12:21:35 +0530

    exception

---
+++ first_excep.java
@@ -0,0 +1,19 @@
+package info.excep;
+
+public class first_excep {
+public void stuff()
+{
+	do_stuff();
+}
+public void do_stuff()
+{
+	int a=14/2;
+	System.out.println("hello");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+first_excep fe=new first_excep();
+fe.stuff();
+	}
+
+}
